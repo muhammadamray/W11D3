6ded0989b09e53d050de9fa5b6d39feb894cc2c3
Naim Miah <mmiah@fordham.edu>
2023-09-06 15:16:24 -0400

    fixed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import configureStore from "./store";
 import "./index.css";
 import App from "./App";
+import configureStore from "./store/index";
 
 const store = configureStore();
 
