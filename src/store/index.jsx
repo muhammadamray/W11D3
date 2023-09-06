da07116211122cb9c4ba7c9c8934a4c406761671
Naim Miah <mmiah@fordham.edu>
2023-09-06 15:26:54 -0400

    yes

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,18 +1,18 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import produceReducer from "./produce";
+const rootReducer = combineReducers({
+  produce: produceReducer,
+});
 
-const rootReducer = combineReducers({});
-
-let enhancer;
-
-if (process.env.NODE_ENV !== "production") {
-  const logger = require("redux-logger").default;
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  enhancer = composeEnhancers(applyMiddleware(logger));
+function configureStore() {
+  const middlewares = [];
+  const enhancer = compose(
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  );
+  const store = createStore(rootReducer, enhancer);
+  return store;
 }
-
-const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
-};
-
 export default configureStore;
